b7196b223a699820987e13eed12216cfd9fd70ee
Austin Paquette <austin@paquette.io>
2018-07-25 23:44:02 -0600

    create a gatsby website infrastructure

--- README.md
+++
@@ -1,2 +0,0 @@
-# WIP
-A fun website
---
+++ gatsby-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  siteMetadata: {
+    title: 'WIP Website',
+    description: 'A work-in-progress',
+  },
+  plugins: ['gatsby-plugin-react-helmet'],
+}
